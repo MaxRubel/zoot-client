154874664afa96fe62eb1e386d35d664f03c25c5
MaxRubel <maxrubel1@gmail.com>
2024-06-06 20:15:52 -0500

    nice

--- src/api/rooms.js
+++ src/api/rooms.js
@@ -2,15 +2,15 @@ const endpoint = "http://localhost:8080";
 
 const createNewRoom = (payload) =>
   new Promise((resolve, reject) => {
-    fetch(`${endpoint}/newRoom`, {
+    fetch(`${endpoint}/rooms`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(payload),
     })
-      .then((response) => response.json())
-      .then((data) => resolve(data))
+      .then((res) => res.text())
+      .then((body) => resolve(body))
       .catch(reject);
   });
 
---
+++ src/components/NewRoomForm.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { navigate } from "svelte-routing";
+  import { createNewRoom } from "../api/rooms";
+
+  let name = "";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createNewRoom({ name }).then((roomId) => {
+      navigate(`/room/${roomId}`);
+    });
+  };
+</script>
+
+<main>
+  <form on:submit={handleSubmit}>
+    <div class="left">Room Name:</div>
+    <input type="text" bind:value={name} />
+    <button type="submit">Create</button>
+  </form>
+</main>
+
+<style>
+  .left {
+    text-align: left;
+  }
+</style>
