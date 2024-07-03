8df4a467936c0e5a8374f217d59191a6500b9c08
MaxRubel <maxrubel1@gmail.com>
2024-07-02 21:02:40 -0500

    rm console loggggs

--- utils/ws/checkPeerConnection.js
+++ utils/ws/checkPeerConnection.js
@@ -1,10 +1,8 @@
 export function checkPeerConnection(peersObj, senderId, myId) {
   if (peersObj[senderId]) {
-    console.log("already connected to: ", senderId);
     return true;
   }
   if (peersObj[senderId] === myId) {
-    console.log("this is my own request");
     return true;
   }
   return false;
